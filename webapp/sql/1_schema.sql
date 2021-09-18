7945de5dbbe6aa5243efad60ab712629174bf2b7
Masatoshi Kawazoe (acidlemon) <acidlemon@beatsync.net>
2021-09-18 12:37:19 +0900

    余計なカンマがどーん

--- webapp/sql/1_schema.sql
+++ webapp/sql/1_schema.sql
@@ -43,7 +43,7 @@ CREATE TABLE `registrations`
     `user_id`   CHAR(26) CHARACTER SET latin1,
     PRIMARY KEY (`course_id`, `user_id`),
     CONSTRAINT FK_registrations_course_id FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`),
-    CONSTRAINT FK_registrations_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+    CONSTRAINT FK_registrations_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
 
 CREATE TABLE `classes`
