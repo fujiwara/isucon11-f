6bc0f4ea68f61e2987744df09995c54d8ab74847
fujiwara <fujiwara.shunichiro@gmail.com>
2021-09-18 02:11:35 +0000

    fix deploy

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -2,6 +2,7 @@
 
 set -xe
 cd go && make && cd -
+now=$(date +%Y%m%d-%H%M%S)
 
 for h in 1 2 3
 do
@@ -17,7 +18,13 @@ do
    ssh isucon${h} sudo mkdir -p /tmp/etc/nginx 
    ssh isucon${h} sudo rsync -av /tmp/etc/nginx/ /etc/nginx/
    ssh isucon${h} sudo touch /var/log/nginx/access.log
-   ssh isucon${h} sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.$(date +%Y%m%d-%H%M%S)
+   ssh isucon${h} sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.$now
    ssh isucon${h} sudo systemctl restart nginx
 done
 
+for h in 1 2
+do
+   ssh isucon${h} sudo touch /var/log/mysql/mysql-slow.log
+   ssh isucon${h} sudo mv /var/log/mysql/mysql-slow.log /var/log/mysql/mysql-slow.log.$now
+   ssh isucon${h} sudo mysqladmin flush-logs
+done
